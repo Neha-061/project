58a99a637e53ff567555497fd2f7e37a3b3f34c8
vishalminj <vishalminj667@gmail.com>
2026-03-26 20:33:49 +0530

    Setting up main API

--- app/backend/routes.py
+++ app/backend/routes.py
@@ -4,16 +4,18 @@ import os
 import numpy as np
 import cv2
 from services import run_pipeline
-
+import time
 
 
 router = APIRouter()
+
+UPLOAD_FOLDER = "backend/images"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
 @router.get("/")
 def root():
     return {"message": "ok"}
-
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @router.post("/upload")
@@ -46,6 +48,45 @@ async def detect(request: Request, file: UploadFile = File(...)):
     mp_face = request.app.state.mp_face
     mp_hands = request.app.state.mp_hands
 
-    result = run_pipeline(img, model, mp_face, mp_hands)
+    req_id = str(int(time.time()*1000))
+    save_dir = os.path.join(UPLOAD_FOLDER, req_id)
+    result = run_pipeline(img, model, mp_face, mp_hands,save_dir)
+
+    return result
+
+
+@router.post("/ask")
+async def analyze(request: Request,file: UploadFile = File(...)):
+
+    contents = await file.read()
+    req_id = str(int(time.time()*1000))
+    save_dir = os.path.join(UPLOAD_FOLDER, req_id)
+    os.makedirs(save_dir, exist_ok=True)
+    f_name = file.filename
+
+    """
+    Uploading the main images and saving it
+    """
+    file_path = os.path.join(save_dir, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+
+    """
+    Running the yolo model
+    """
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    model = request.app.state.model
+    mp_face = request.app.state.mp_face
+    mp_hands = request.app.state.mp_hands
+    result = run_pipeline(img, model, mp_face, mp_hands,save_dir)
+
+
+    """
+    Generation Effiecient Net score
+    """
 
     return result
